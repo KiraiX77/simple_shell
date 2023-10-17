889a8dfc146f1129faf5e9f845d95bdf8c3f721e
KiraiX77 <kiraix77@gmail.com>
2023-10-18 00:39:01 +0100

    error_handling.c

--- error_handling.c
+++ error_handling.c
@@ -25,7 +25,7 @@ void print_no_permission(int count, const char *command_name)
 void print_error_message(int count, const char
 *command_name, const char *message)
 {
-    char *shell_name = _strdup(SHELL_NAME);
+	char *shell_name = _strdup(SHELL_NAME);
 
 	if (shell_name == NULL)
 	{	perror("strdup");
